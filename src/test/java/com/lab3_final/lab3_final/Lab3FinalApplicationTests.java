aca3f27509cf7133f4081e706a6034d08d1dd71c
Sofii2301 <sofialvarezrodero97@gmail.com>
2024-07-11 11:18:31 -0300

    first commit

---
+++ src/test/java/com/lab3_final/lab3_final/Lab3FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab3_final.lab3_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
